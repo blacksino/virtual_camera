fix a normalization bug add test code for Shape Context descriptor. ShapeContext descriptor is NOT mirror translation invariant,need to improve.

rough_registraion/ShapeContextMatching.py
@@ -2,6 +2,8 @@ import math, cv2
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 import tps
+import json
+
 
 class Point:
     def __init__(self, x, y):
@@ -36,7 +38,7 @@ class Shape:
         for point in shape:
             self.shape_pts.append(Point(point[0], point[1]))
         self.shape_contexts = self.get_shape_contexts()
-
+        # self.log_polar_points = [point.cart2logpolar() for point in self.shape_pts]
     def get_shape_contexts(self, angular_bins=12, radious_bins=None):
         '''
             angular_bins -> number of bins for angle.
@@ -84,10 +86,10 @@ class Shape:
                               if added to Q -> m)
         '''
         def normalize_histogram(hist, total):
-
+            new_hist = hist.copy()
             for i in range(hist.shape[0]):
-                hist[i] /= float(total)
-            return hist
+                new_hist[i] /= float(total)
+            return new_hist
 
         def shape_context_cost(nh1, nh2):
             '''
@@ -148,7 +150,7 @@ class Shape:
                       from Pshape matched to
                       point i from Qshape.
         '''
-        cost_matrix, flag = self.get_cost_matrix(Q)
+        cost_matrix, flag = self.get_cost_matrix(Q,beta=0.9)
         perm = linear_sum_assignment(cost_matrix)[1]
         Pshape = np.array(self.shape)
         Qshape = np.array(Q.shape)
@@ -297,3 +299,8 @@ class utils:
         for i in range(x.shape[0]):
             shape.append([x[i], y[i]])
         return shape
+
+
+if __name__ == '__main__':
+    with open('/home/SENSETIME/xulixin2/registration.json') as f:
+        data = json.load(f)

rough_registraion/solveEPnP.py
@@ -67,51 +67,51 @@ def rot_error(r_gt,r_est):
     #公式计算结果单位为弧度，转成角度返回
     return dis*180/math.pi
 
-if init_frame:
-    json_path = '/data/endoscope/simulation_data/17:39:29/registration.json'
-    registration_json_path = '/data/endoscope/simulation_data/17:39:29/registration.json'
-    info = dict()
-    registration_info = dict()
-
-    with open(json_path, 'r') as f:
-        info = json.load(f)
-
-    with open(registration_json_path, 'r') as f:
-        registration_info = json.load(f)
-
-    gt_extrinsics = np.array(info['extrinsics'])
-    K = np.array(info['intrinsics'])
-    K[-1][-1] = 1
-    scene_points = np.array(registration_info['scene_points'])
-    # contour_points = np.array(registration_info['coutour'])
-
-    image = cv2.imread('/data/endoscope/simulation_data/17:39:29/registration.png')
-    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-
-    key_points_mask = np.zeros(image.shape[:-1])
-    key_points_mask[(image[:,:,0]>150)&(image[:,:,1]<100)&(image[:,:,-1]<100)] = 1
-    key_points_mask = label(key_points_mask)
-    regions = regionprops(key_points_mask)
-
-    key_points = np.array([each_region.centroid for each_region in regions])
-    key_points = key_points[:,[1,0]] # 交换xy
-    w, h = 2 * K[0, 2], 2 * K[1, 2]
-    # key_points[:,0] = w-1 - key_points[:,0]
-    # key_points[:,1] = h-1 - key_points[:,1]
-    key_points = key_points[key_points[:, 0].argsort(), :]
-    transform = cv2.solvePnP(scene_points.astype(np.float64),key_points.astype(np.float64),
-                             K.astype(np.float64),distCoeffs=None,flags=cv2.SOLVEPNP_EPNP) # 当可用点仅有四点时。
-
-    rot_matrix = cv2.Rodrigues(transform[1])[0].T
-    translation = -rot_matrix @ transform[2]
-    homogeneous_transform = np.zeros((4,4))
-    homogeneous_transform[:3,:3] = rot_matrix
-    homogeneous_transform[:3, 3] = translation.flatten()
-    homogeneous_transform[-1,-1] = 1
-    translation_error = translation.flatten() - gt_extrinsics[:3,3]
-    angle_error = rot_error(gt_extrinsics[:3,:3],rot_matrix)
-    print(f'translation error is {round(np.linalg.norm(translation_error),3)} mm.')
-    print(f'angle error is {180 - round(angle_error,3)} degree.')
+# if init_frame:
+#     json_path = '/data/endoscope/simulation_data/17:39:29/registration.json'
+#     registration_json_path = '/data/endoscope/simulation_data/17:39:29/registration.json'
+#     info = dict()
+#     registration_info = dict()
+#
+#     with open(json_path, 'r') as f:
+#         info = json.load(f)
+#
+#     with open(registration_json_path, 'r') as f:
+#         registration_info = json.load(f)
+#
+#     gt_extrinsics = np.array(info['extrinsics'])
+#     K = np.array(info['intrinsics'])
+#     K[-1][-1] = 1
+#     scene_points = np.array(registration_info['scene_points'])
+#     # contour_points = np.array(registration_info['coutour'])
+#
+#     image = cv2.imread('/data/endoscope/simulation_data/17:39:29/registration.png')
+#     image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+#
+#     key_points_mask = np.zeros(image.shape[:-1])
+#     key_points_mask[(image[:,:,0]>150)&(image[:,:,1]<100)&(image[:,:,-1]<100)] = 1
+#     key_points_mask = label(key_points_mask)
+#     regions = regionprops(key_points_mask)
+#
+#     key_points = np.array([each_region.centroid for each_region in regions])
+#     key_points = key_points[:,[1,0]] # 交换xy
+#     w, h = 2 * K[0, 2], 2 * K[1, 2]
+#     # key_points[:,0] = w-1 - key_points[:,0]
+#     # key_points[:,1] = h-1 - key_points[:,1]
+#     key_points = key_points[key_points[:, 0].argsort(), :]
+#     transform = cv2.solvePnP(scene_points.astype(np.float64),key_points.astype(np.float64),
+#                              K.astype(np.float64),distCoeffs=None,flags=cv2.SOLVEPNP_EPNP) # 当可用点仅有四点时。
+#
+#     rot_matrix = cv2.Rodrigues(transform[1])[0].T
+#     translation = -rot_matrix @ transform[2]
+#     homogeneous_transform = np.zeros((4,4))
+#     homogeneous_transform[:3,:3] = rot_matrix
+#     homogeneous_transform[:3, 3] = translation.flatten()
+#     homogeneous_transform[-1,-1] = 1
+#     translation_error = translation.flatten() - gt_extrinsics[:3,3]
+#     angle_error = rot_error(gt_extrinsics[:3,:3],rot_matrix)
+#     print(f'translation error is {round(np.linalg.norm(translation_error),3)} mm.')
+#     print(f'angle error is {180 - round(angle_error,3)} degree.')
     # pc = o3d.open3d.geometry.PointCloud()
     # pc.points = o3d.utility.Vector3dVector(np.array(contour_points))
     # o3d.io.write_point_cloud("/data/endoscope/simulation_data/contour.ply", pc, write_ascii=True)