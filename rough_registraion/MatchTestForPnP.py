translation invariance and rotation invariance conflict bug fixed.

rough_registraion/MatchTestForPnP.py
@@ -32,13 +32,16 @@ def scatter_logpolar_mpl(ax, theta, r):
     ax.set_title('log-polar matplotlib')
 
 
-def plot_points_in_log_polar(shape_source, shape_target, style='cartesian', result=None):
-    source_points = [[each_point.x, each_point.y] for each_point in shape_source.shape_pts]
-    target_points = [[each_point.x, each_point.y] for each_point in shape_target.shape_pts]
+def plot_points_in_log_polar(shape_source, shape_target, style='cartesian', result=None,use_shape=False):
+    if use_shape:
+        source_points = [[each_point.x, each_point.y] for each_point in shape_source.shape_pts]
+        target_points = [[each_point.x, each_point.y] for each_point in shape_target.shape_pts]
 
-    source_points = np.array(source_points)
-    target_points = np.array(target_points)
-
+        source_points = np.array(source_points)
+        target_points = np.array(target_points)
+    else:
+        source_points = shape_source
+        target_points = shape_target
     if style == 'polar':
         center_source = np.mean(source_points, axis=0)
         center_target = np.mean(target_points, axis=0)
@@ -56,9 +59,9 @@ def plot_points_in_log_polar(shape_source, shape_target, style='cartesian', resu
         fig, ax = plt.subplots(1, 2, figsize=(10, 5))
         # set dpi
         fig.set_dpi(500)
-        ax[0].scatter(source_points[:, 0], source_points[:, 1], s=0.2)
+        ax[0].scatter(source_points[:, 0], source_points[:, 1], s=1)
         ax[0].set_title('source points')
-        ax[1].scatter(target_points[:, 0], target_points[:, 1], s=0.2)
+        ax[1].scatter(target_points[:, 0], target_points[:, 1], s=1)
         ax[1].set_title('target points')
         if result is not None:
             # add ConnectionPatch to connect the two points

rough_registraion/Registration.py
@@ -34,20 +34,24 @@ def plot_log_polar_for_sc(shape_context,vectorized_shape_context):
 
 if __name__ == '__main__':
     extrinsics, scene_points = load_json("/home/SENSETIME/xulixin2/registration.json")
-    image_points = np.loadtxt("/data/image_points.txt")
+    # image_points = np.loadtxt("/data/image_points.txt")
+    image_points = np.loadtxt("/data/image_points_wo_sampled.txt")
     projected_object_points = np.loadtxt("/data/projected_points.txt")
     # reverse image_points and projected_object_points along the y axis
     image_points[:, 1] = - image_points[:, 1]
     projected_object_points[:, 1] = - projected_object_points[:, 1]
 
-    image_points[:,0] = fx * image_points[:,0]  + cx
-    image_points[:,1] = fy * image_points[:,1]  + cy
-    projected_object_points[:,0] = fx * projected_object_points[:,0]  + cx
-    projected_object_points[:,1] = fy * projected_object_points[:,1]  + cy
+    image_points[:, 0] = fx * image_points[:, 0] + cx
+    image_points[:, 1] = fy * image_points[:, 1] + cy
+    projected_object_points[:, 0] = fx * projected_object_points[:, 0] + cx
+    projected_object_points[:, 1] = fy * projected_object_points[:, 1] + cy
 
     projected_object_points_using_extrinsics = project_points(scene_points, extrinsics, K)
-    image_points = image_points[np.argsort(image_points[:, 0])]
-    projected_object_points = projected_object_points[np.argsort(projected_object_points[:, 0])]
+    # projected_object_points_using_extrinsics = apply_affine_on_points(projected_object_points_using_extrinsics, np.pi/6)
+    # image_points = image_points[np.argsort(image_points[:, 0])]
+    # projected_object_points = projected_object_points[np.argsort(projected_object_points[:, 0])]
+    # projected_object_points_using_extrinsics =projected_object_points_using_extrinsics[np.argsort(projected_object_points_using_extrinsics[:, 0])]
+
 
     fig, ax = plt.subplots(1, 3, figsize=(15, 5))
     ax[0].scatter(image_points[:, 0], image_points[:, 1], c='r', s=1)
@@ -60,32 +64,33 @@ if __name__ == '__main__':
     plt.show()
 
     # try to solve pnp using opencv
-
     shape_image = SC.Shape(image_points)
     shape_projected = SC.Shape(projected_object_points)
-    match_result,perm = shape_image.matching(shape_projected,with_perm=True)
-    match_result_inv = shape_projected.matching(shape_image)
+    shape_gt= SC.Shape(projected_object_points_using_extrinsics)
 
-    _,rvec, tvec =cv2.solvePnP(scene_points, match_result_inv[1],K,distCoeffs=None,flags=cv2.SOLVEPNP_SQPNP,useExtrinsicGuess=False)
-    cv2.solvePnPRansac(scene_points, match_result_inv[1], K, distCoeffs=None, useExtrinsicGuess=False,reprojectionError=1,confidence=0.99,maxIterations=100,minInlierRatio=0.1,inlierMask=None)
+    match_result,p1 = shape_image.matching(shape_projected,1)
+    match_result_inv,p2 = shape_projected.matching(shape_image,1)
+    match_result_gt,p3 = shape_image.matching(shape_gt,1)
 
-    new_points,_ = cv2.projectPoints(scene_points, rvec, tvec, K, distCoeffs=None)
+    # _,rvec, tvec =cv2.solvePnP(scene_points, match_result_inv[1],K,distCoeffs=None,flags=cv2.SOLVEPNP_SQPNP,useExtrinsicGuess=False)
+    # cv2.solvePnPRansac(scene_points, match_result_inv[1], K, distCoeffs=None, useExtrinsicGuess=False,reprojectionError=1,confidence=0.99)
+    # new_points,_ = cv2.projectPoints(scene_points, rvec, tvec, K, distCoeffs=None)
+    #
+    # plt.scatter(new_points[:,0,0],new_points[:,0,1],c='r',s=1,label='solved by PnP')
+    # plt.scatter(image_points[:,0],image_points[:,1],c='b',s=1,label='gt')
+    # # make some annotations
+    # plt.legend()
+    # plt.show()
+    #
+    # # calculate hausdorff distance between new_points and projected_points
+    # sc_test_1 = shape_image.shape_contexts[0].reshape(6,-1)
+    # sc_test_2 = shape_projected.shape_contexts[0].reshape(6,-1)
+    # fig,ax = plt.subplots(1,2,figsize=(10,5),dpi=500)
+    # ax[0].imshow(sc_test_1.reshape(6,-1),cmap='gray')
+    # ax[1].imshow(sc_test_2.reshape(6,-1),cmap='gray')
+    # plt.show()
+    plot_points_in_log_polar(image_points,projected_object_points,result=match_result)
+    plot_points_in_log_polar(image_points,projected_object_points_using_extrinsics,result=match_result_gt)
 
-    plt.scatter(new_points[:,0,0],new_points[:,0,1],c='r',s=1,label='solved by PnP')
-    plt.scatter(projected_object_points[:,0],projected_object_points[:,1],c='b',s=1,label='gt')
-    # make some annotations
-    plt.legend()
-    plt.show()
-
-    sc_test_1 = shape_image.shape_contexts[0].reshape(6,-1)
-    sc_test_2 = shape_projected.shape_contexts[0].reshape(6,-1)
-    fig,ax = plt.subplots(1,2,figsize=(10,5),dpi=500)
-    ax[0].imshow(sc_test_1.reshape(6,-1),cmap='gray')
-    ax[1].imshow(sc_test_2.reshape(6,-1),cmap='gray')
-    plt.show()
-
-
-
-    plot_points_in_log_polar(shape_image,shape_projected,result=match_result)
 
 